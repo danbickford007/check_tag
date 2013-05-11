82872882db86e28416bd904b2f9923a29888e2b2
Tim <timbinous@gmail.com>
2013-05-10 23:48:07 -0400

    trying raw

--- lib/check_tag.rb
+++ lib/check_tag.rb
@@ -6,6 +6,6 @@ end
 
 module CheckTag
   def check_tag(id, check=true, uncheck=false, defaulted=false)
-    "#{hidden_field_tag(id, uncheck)} #{check_box_tag(id, check, defaulted)}"
+    raw "#{hidden_field_tag(id, uncheck)} #{check_box_tag(id, check, defaulted)}"
   end
 end
